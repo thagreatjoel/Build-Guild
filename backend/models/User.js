4d045872de467a6321880816715350c8438ff1c1
Joel Jo <108486336+thagreatjoel@users.noreply.github.com>
2026-04-01 16:53:23 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, default: '' },
+  username: { type: String, default: '', unique: true, sparse: true },
   email: { type: String, required: true, unique: true },
   phone: { type: String, default: '' },
   qrToken: { type: String, default: '' },
   checkedIn: { type: Boolean, default: false },
   checkedInAt: { type: Date, default: null },
+  score: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
   loggedIn: { type: Boolean, default: false },
   lastLogin: { type: Date, default: null }
