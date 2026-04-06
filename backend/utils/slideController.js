5e8c1bdebfbc86ce40c16e564ba3be14703ec3e1
Joel Jo <108486336+thagreatjoel@users.noreply.github.com>
2026-04-06 16:25:08 +0530

    Update slideController.js

--- backend/utils/slideController.js
+++ backend/utils/slideController.js
@@ -1,16 +1,31 @@
-let actions = [];
+let clients = [];
+
+exports.events = (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  res.flushHeaders();
+
+  clients.push(res);
+
+  req.on('close', () => {
+    clients = clients.filter(c => c !== res);
+  });
+};
+
+function sendEvent(action) {
+  clients.forEach(client => {
+    client.write(`data: ${JSON.stringify({ action })}\n\n`);
+  });
+}
 
 exports.next = (req, res) => {
-  actions.push("next");
+  sendEvent("next");
   res.json({ ok: true });
 };
 
 exports.prev = (req, res) => {
-  actions.push("prev");
+  sendEvent("prev");
   res.json({ ok: true });
-};
-
-exports.status = (req, res) => {
-  const action = actions.shift() || null;
-  res.json({ action });
 };
