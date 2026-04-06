b0f69269e897e620a1de347059c7af0895c7b09e
Joel Jo <108486336+thagreatjoel@users.noreply.github.com>
2026-04-06 16:25:31 +0530

    Update slide.js

--- backend/routes/slide.js
+++ backend/routes/slide.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const slide = require('../utils/slideController');
 
+router.get('/events', slide.events);
 router.get('/next', slide.next);
 router.get('/prev', slide.prev);
-router.get('/status', slide.status);
 
 module.exports = router;
